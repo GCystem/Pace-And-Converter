79523fde82cb3cb6c454e204b89cbe63d352ce05
User <User@wireless-nat-inside.uiowa.edu>
2015-08-31 13:23:56 -0500

    Initial Commit

---
+++ Training Log And ConverterTests/Training_Log_And_ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Training_Log_And_ConverterTests.swift
+//  Training Log And ConverterTests
+//
+//  Created by User on 8/31/15.
+//  Copyright (c) 2015 Kevin Lewis. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Training_Log_And_ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
